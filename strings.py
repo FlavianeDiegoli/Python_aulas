85180c9178b14968d05fcf19fa3c035aa2056e4d
flaviane <flavi_4rs@hotmail.com>
2023-06-19 22:15:08 -0300

    Nova

---
+++ strings.py
@@ -0,0 +1,15 @@
+
+nome = "Flaviane"
+sobrenome = "Diegoli"
+
+print(nome + sobrenome);
+
+
+frase = "Olá, Mundo!";
+print("String original: ");
+print(frase);
+print();
+
+print("Acessando caracteres individuais: ");
+print("Primeiro caractere: ", frase[0]);
+print("Ultimo caractere ", frase[-1]);
---
+++ variavel.py
@@ -0,0 +1,9 @@
+#nome = "Flaviane"
+#print(nome)
+
+
+# 77nome = "Flaviane"
+
+# mai$ = "Main"
+
+class nome: "Python"
