5276de5bb1902a80d44dd15f3d6db8fec3d24009
flaviane <flavi_4rs@hotmail.com>
2023-06-22 16:24:12 -0300

    Atividade

---
+++ Excericio.py
@@ -0,0 +1,18 @@
+nome ="Flaviane";
+sobrenome=" Soares Diegoli"
+
+print("nome da completa");
+print(nome + sobrenome);
+
+
+nome="Flaviane";
+idade =" 34 anos ";
+
+print( nome + idade )
+
+Natural = "Pelotas RS "
+Mora = " Brusque Sc "
+
+print(f"Eu nasceu a natural e muda é mora ")
+print( Natural + Mora );
+
