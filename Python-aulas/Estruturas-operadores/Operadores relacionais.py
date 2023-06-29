f3471a9ba595ba4bfb3e107b6a319d2e828420d3
flaviane <flavi_4rs@hotmail.com>
2023-06-29 20:24:28 -0300

    Nova pasta commit

---
+++ Python-aulas/Estruturas-operadores/Operadores relacionais.py
@@ -0,0 +1,26 @@
+"""
+Operadores relacionais
+"""
+x = 5;
+y = 7;
+
+#Maior que 
+
+print(x > y); #false
+print()
+
+#Menor que 
+print(x < 7) #true
+print();
+
+#Igualdade
+print(x == y); #false
+print()
+
+#Maior ou Igual a
+print(x <= y); #true
+print()
+
+#Diferente de
+print(x !=y); #true
+print();
