d15e93eb50cc66d784e5160e5c8df93a6db7006e
flaviane <flavi_4rs@hotmail.com>
2023-06-30 18:55:48 -0300

    exercio 1 nova

---
+++ Python-aulas/Atividades/Excericio 1 .py
@@ -0,0 +1,75 @@
+frutas =['maça','banana',' laranja','abacaxi','melancia'];
+print(frutas);
+print();
+
+#Adicione a fruta
+frutas.append('uva');
+
+#emova a fruta
+frutaRemovida = frutas.remove("banana");
+
+frutaRemovida = frutas.pop(2);
+print(frutas);
+print(frutaRemovida);
+
+#uma variável chamada
+fruta_selecionada = {
+  'maçã'      : 3,
+  'banana'    : 6,
+  'laranja'   : 8,
+  'abacaxi'   : 9,
+  'melancia'  : 10
+};
+
+print();
+print("Valores", fruta_selecionada.values());
+print(fruta_selecionada.items());
+
+#tupla chamada
+tuplaCores = ['vermelho', 'azul', 'verde', 'amarelo' , 'roxo'];
+print(tuplaCores);
+print();
+
+#uma variável chamada
+cor_selecionada = {
+    'vermelho'   : 1,
+    ' azul '     : 2,
+    'verde'      : 3,
+    'amarelo'    : 4,
+    'roxo'       : 5,
+};
+print();
+print("Valores", cor_selecionada.values());
+print(cor_selecionada.items());
+
+# esta dificil a erro ?
+frutas[1] = "laranja";
+print("Tamanho 1: ", len(frutas));
+frutas.append("abacaxi");
+print(frutas);
+
+aluno = {
+     'nome'      : 'Flaviane',
+     'sobrenome' : 'Diegoli ',
+     'anos'      : 34  ,
+     'cidade'    :'Brusque SC'
+}
+print(aluno);
+print();
+
+print("Idade do aluno : ", aluno["anos"]);
+print();
+
+generoAluno = ["feminino"];
+print("Gênero: ", generoAluno);
+print();
+
+print("aluno e chave genero  " ,aluno ["anos"]);
+print();
+
+
+elementoRemovido = aluno.pop("cidade");
+print(elementoRemovido);
+print();
+print("Dicionário atualizado:",aluno);
+
