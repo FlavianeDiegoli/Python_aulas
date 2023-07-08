4e84ba3725448dbfdad7ff8ca47de85529a0b7f0
flaviane <flavi_4rs@hotmail.com>
2023-07-08 17:58:07 -0300

    commit nova a muda e atividade .

---
+++ Python-aulas/Atividades/ExcericioJogos.py
@@ -0,0 +1,18 @@
+# Este é o jogo adivinhe o número.
+import random
+tentativas = 0
+print("Olá! Qual é o seu nome?")
+meuNome = input()
+numero = random.randint(1, 100)
+print("Bem, " + meuNome + ", eu estou pensando em um número entre 1 e 100.")
+while tentativas < 6:
+    print("Tente adivinhar!") # Há quatro espaços na frente do print.
+    palpite = input()
+    palpite = int(palpite)
+    tentativas = tentativas + 1
+    if palpite < numero:
+        print(" é muito baixo!") # Há oito espaços na frente do print.
+    if palpite > numero:
+        print("é muito alto!")
+    if palpite == numero:
+        break
--- Python-aulas/Lacos_respeitcao/while.py
+++ Python-aulas/Lacos_respeitcao/while.py
@@ -1,13 +1,12 @@
-# #Exemplo 1:  Contagem regressiva de 10 a 1
+# 1 - utlilizando um loop "while", imprimima os numeros de 1 a 0
 
 contador = 10;
 
 while contador >= 1:
   print(contador);
   contador -= 1;
-  
-#Exemplo 2: Leitura de notas de alunos até que uma nota negativa seja inserida
 
+# 2 - utlilizando um loop "for", imprimima os numeros de 1 a 0 
 notas = [];
 nota = float(input("Digite uma nota (-1 para sair): "));
 
@@ -16,9 +15,8 @@ while nota >= 0:
   nota = float(input("Digite uma nota (-1 para sair): "));
   
 print(notas); 
-
-#Exemplo 3: Verificação de senha correta
-
+    
+# 3 - utlilizando um loop "while", clacule a soma dos numeros de 1 a 100
 senha = input("Informe uma senha: ");
 contador = 0;
 senhaBloqueada = False;
@@ -36,17 +34,19 @@ if(senhaBloqueada):
 else:
   print("Senha correta!");
 
-#Exemplo 4: Impressão dos primeiros N numeros pares
+
+# 4 - utlilizando um loop "for", clacule a soma dos numeros de 1 a 100
 quantidade = int(input("Informe a quantidade de numeros pares a serem impressos: "));
 contador = 1;
+
 while quantidade > 0:
   if contador % 2 == 0 :
     print(contador);
     quantidade -= 1;
   contador += 1;
-  
-#Jogo da adivinhação
-
+    
+    
+# 5 - utlilizando um loop "while", imprima os numeros de 1 a 20
 numeroSecreto = 42;
 palpite = int(input("Digite um número: "));
 
@@ -56,11 +56,67 @@ while palpite != numeroSecreto:
 
 print("Parabéns! Você acertou o palpite!");
 
-#Exemplo 6: Impressão de uma sequência de caracteres até que a palavra "sair" seja digitada
 
+# 6 - utlilizando um loop "for", imprima os numeros pares de 1 a 20
 palavra = input("Digite uma plavra ('sair' para encerrar): ");
 palavra = palavra.lower();
 while palavra != 'sair' :
   print(palavra);
   palavra = input("Digite uma plavra ('sair' para encerrar)");
   palavra = palavra.lower();
+
+#Exemplo: implementação de meu opções.
+opcao = 0;
+
+while opcao != 4:
+  print("Menu: ");
+  print("1. opção 1");
+  print("2. opção 2");
+  print("3. opção 3");
+  print("4. Sair");
+  
+  opcao = int(input("Informa opção escolhida: "));
+  if opcao == 1:
+    print("Opção 1 selecionada!");
+  elif opcao == 2:
+    print("Opção 2 selecionada!");
+  elif opcao == 3:
+    print("Opção 3 selecionada!");
+  elif opcao == 4:
+   print("Saindo...");
+  break;
+else:
+  print("Opção inválida, tente novamente");
+  
+  
+  #emulando do while 
+  
+  palavraSecreta = "pyhton";
+  conuter = 0;
+  
+  while True:
+    palavra = input("Informe a palavra secreta").lower();
+    contador +- 1;
+    
+    if palavra == palavraSecreta:
+      print("Você acertou a palavra!");
+      break;
+    if(palavra != palavraSecreta and contador > 7):
+      print("Voce atingiu o limite de tentativas!");
+
+# 7 - utlilizando um loop "white", inverta uma string digitada pelo usuario
+
+
+
+
+# 8 - utlilizando um loop "for", # verifique se uma palavra digitada pelo usuario 
+# é um palíndromo(lê-se da mesma forma de tras para frente).
+
+
+
+#9 - utlilizando um loop "while", encontre o menor numeros inteiro cujo quadrado seja
+#maior do que 100. 
+
+
+
+# 10 - utlilizando um loop "for", imprima os elementos de uma lista em ordem inversa 
